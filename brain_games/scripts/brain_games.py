5d13d0a4dd9424e4d926e41cef829c0704056806
Andrey Larin <larin.projects@gmail.com>
2020-06-21 13:10:39 +0300

    Fix brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
-from brain_games.cli import GREETING, welcome_user
+from brain_games.cli import GREETING, welcome_user, ask_name
 
 
 def main():
     print(GREETING)
     print()
-    print(welcome_user())
+    name = ask_name()
+    print(welcome_user(name))
 
 
 if __name__ == '__main__':
